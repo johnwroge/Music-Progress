d9d836c05eabcffc646a83bb3027eb941d2fe276
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-01-14 17:59:51 -0500

    styling title page

--- README.md
+++ README.md
@@ -2,23 +2,4 @@
 A way for musicians to create helpful practice sheets. The idea of this app was developed 
 from The Musicians Way by Gerald Klickstein. 
 
-New Material 
-	Divide into Sections
-	Establish Interpretative/technical plan
-	slow tempo
-Developing Material
-	Refine interpretation
-	increase tempo
-	memorize
-Performance Material
-	practice performing
-	maintain memory
-	renew and innovate
-
-Technique
-
-	Scales, arpeggios, diction, instrument specific work
-
-Musicianship
-
-	sight-reading, theory/ear training, composition/improvisation, listening/study
+To start, enter information on each page. 
--- src/client/Title.tsx
+++ src/client/Title.tsx
@@ -4,14 +4,17 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 export default function Title (){
     return (
-        <div className='todo'>
-           <h1 > Muse Tracker </h1>
-           <h4 className='main-par'> Fill out the included text areas to create a new practice sheet </h4>
-           <Link
-                to = "/todo"
-            >
-            <button className="next">Next Page</button>
-            </Link>
+        
+        <div className = "outer-main"> 
+            <div className='todo'>
+            <h1 className='title'> Muse Tracker </h1>
+            <h4> Fill out the text areas to create a new practice sheet! </h4>
+            <Link
+                    to = "/todo"
+                >
+                <button className="next">Next Page</button>
+                </Link>
+            </div>
         </div>
     )
 }
