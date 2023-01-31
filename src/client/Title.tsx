7ab1f62456634b94bd0a4ed20ed435021153a393
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-01-31 00:24:21 -0500

    styling

--- src/client/Title.tsx
+++ src/client/Title.tsx
@@ -6,9 +6,11 @@ export default function Title (){
     return (
         
         <div className = "outer-main"> 
-            <div className='todoTitle'>
-            <h1 className='title'> Welcome to Music Progress </h1>
+
+            <h1 className='title'> Welcome to<span>Music Progress </span> </h1>
             <h4> Fill out the text areas to create a new practice sheet! </h4>
+            <div className='todoTitle'>
+            
             <Link
                     to = "/todo"
                 >
