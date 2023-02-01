448c42eb4756a706a8e0a391477f4597114ad0f7
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-01-31 21:18:38 -0500

    added styling

--- src/client/Title.tsx
+++ src/client/Title.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import music from '../../public/music.png'
 
 export default function Title (){
     return (
+        <> 
         
         <div className = "outer-main"> 
-
             <h1 className='title'> Welcome to<span>Music Progress </span> </h1>
-            <h4> Fill out the text areas to create a new practice sheet! </h4>
+            <img src = {music}/>
+         </div>   
+         <p> Fill out the text areas to create a new practice sheet! </p>
             <div className='todoTitle'>
-            
-            <Link
+                <Link
                     to = "/todo"
                 >
                 <button className="next">Next Page</button>
                 </Link>
             </div>
-        </div>
+        </>
     )
 }
